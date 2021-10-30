d4e14203bb9e87fbccdbbb94f34bc6ff90bc35d9
orhan <orhancansu98@gmail.com>
2021-10-30 18:05:19 +0300

    deleted time

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
-import datetime
-
 
 
 class PublishedManager(models.Manager):
